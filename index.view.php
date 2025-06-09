eca3c9be09d3c37b462effff0a0584bfc08af56b
christian-crg <christian-crg@hotmail.com>
2025-06-08 21:15:38 -0600

    Board without CSS

--- index.view.php
+++ index.view.php
@@ -12,5 +12,587 @@
     <h1>
         <?php echo APPNAME; ?>
     </h1>
+<table border="1" height="100%">
+	<caption>Tablero</caption>
+	<tbody>
+		<tr>
+			<td>Groenlandia</td>
+			<td>
+				Japón
+				<br/>
+				$ 250
+			</td>
+			<td>
+				Aduana
+				<br/>
+				Pague $ 100
+			</td>
+			<td>
+				China
+				<br/>
+				$ 250
+			</td>
+			<td>
+				Turquía
+				<br/>
+				$ 220
+			</td>
+			<td>
+				Línea Aérea
+				<br/>
+				Asiática
+				<br/>
+				$ 200
+			</td>
+			<td>
+				Israel
+				<br/>
+				$ 250
+			</td>
+			<td>
+				E-mail
+			</td>
+			<td>
+				India
+				<br/>
+				$ 200
+			</td>
+			<td>
+				Rusia
+				<br/>
+				$ 270
+			</td>
+			<td>
+				Oceanía
+			</td>
+		</tr>
+		<tr>
+			<td>
+				Bandera
+				<br/>
+				Japón
+			</td>
+			<td>
+				Imagen
+				<br/>
+				Aduana
+			</td>
+			<td>
+				Bandera
+				<br/>
+				China
+			</td>
+			<td>
+				Bandera
+				<br/>
+				Turquía
+			</td>
+			<td>
+				Imagen
+				<br/>
+				Línea Aérea
+				<br/>
+				Asiática
+			</td>
+			<td>
+				Bandera
+				<br/>
+				Israel
+			</td>
+			<td>
+				Imagen
+				<br/>
+				E-mail
+			</td>
+			<td>
+				Bandera
+				<br/>
+				India
+			</td>
+			<td>
+				Bandera
+				<br/>
+				Rusia
+			</td>
+		</tr>
+		<tr>
+			<td>
+				Color segmento
+				<br/>
+				asiático dos
+				<br/>
+				Japón
+			</td>
+			<td>
+				Color segmento
+				<br/>
+				asiático dos
+				<br/>
+				China
+			</td>
+			<td>
+				Color segmento
+				<br/>
+				asiático dos
+				<br/>
+				Turquía
+			</td>
+			<td>
+				Color segmento
+				<br/>
+				asiático uno
+				<br/>
+				Israel
+			</td>
+			<td>
+				Color segmento
+				<br/>
+				asiático uno
+				<br/>
+				India
+			</td>
+			<td>
+				Color segmento
+				<br/>
+				asiático uno
+				<br/>
+				Rusia
+			</td>
+		</tr>
+		<tr>
+			<td>
+				Noruega
+				<br/>
+				$250
+			</td>
+			<td>
+				Bandera
+				<br/>
+				Noruega
+			</td>
+			<td>
+				Color segmento
+				<br/>
+				Europa uno
+				<br/>
+				Noruega
+			</td>
+			<td>
+				Mapa
+				<br/>
+				Mundo
+			</td>
+			<td>
+				Color segmento
+				<br/>
+				africano dos
+				<br/>
+				Liberia
+			</td>
+			<td>
+				Bandera
+				<br/>
+				Liberia
+			</td>
+			<td>
+				Liberia
+				<br/>
+				$ 220
+			</td>
+		</tr>
+		<tr>
+			<td>
+				Consulado
+			</td>
+			<td>
+				Imagen
+				<br/>
+				Consulado
+			</td>
+			<td>
+				Color segmento
+				<br/>
+				africano dos
+				<br/>
+				Nigeria
+			</td>
+			<td>
+				Bandera
+				<br/>
+				Nigeria
+			</td>
+			<td>
+				Nigeria
+				<br/>
+				$ 200
+			</td>
+		</tr>
+		<tr>
+			<td>
+				Alemania
+				<br/>
+				$ 270
+			</td>
+			<td>
+				Bandera
+				<br/>
+				Alemania
+			</td>
+			<td>
+				Color segmento
+				<br/>
+				europeo uno
+				<br/>
+				Alemania
+			</td>
+			<td>
+				Imagen
+				<br/>
+				Fax
+			</td>
+			<td>
+				Fax
+			</td>
+		</tr>
+		<tr>
+			<td>
+				Polonia
+				<br/>
+				$ 300
+			</td>
+			<td>
+				Bandera
+				<br/>
+				Polonia
+			</td>
+			<td>
+				Color segmento
+				<br/>
+				europeo uno
+				<br/>
+				Polonia
+			</td>
+			<td>
+				Color segmento
+				<br/>
+				africano dos
+				<br/>
+				Sudán
+			</td>
+			<td>
+				Bandera
+				<br/>
+				Sudán
+			</td>
+			<td>
+				Sudán
+				<br/>
+				$ 180
+			</td>
+		</tr>
+		<tr>
+			<td>
+				Línea aérea
+				<br/>
+				Europea
+				<br/>
+				$ 200
+			</td>
+			<td>
+				Imagen
+				<br/>
+				Línea aérea
+				<br/>
+				Europea
+			</td>
+			<td>
+				Imagen
+				<br/>
+				Línea aérea
+				<br/>
+				Africana
+			</td>
+			<td>
+				Línea aérea
+				<br/>
+				Africana
+				<br/>
+				$ 200
+			</td>
+		</tr>
+		<tr>
+			<td>
+				España
+				<br/>
+				$ 300
+			</td>
+			<td>
+				Bandera
+				<br/>
+				España
+			</td>
+			<td>
+				Color segmento
+				<br/>
+				europeo dos
+				<br/>
+				España
+			</td>
+			<td>
+				Color segmento
+				<br/>
+				africano uno
+				<br/>
+				Etiopía
+			</td>
+			<td>
+				Bandera
+				<br/>
+				Etiopía
+			</td>
+			<td>
+				Etiopía
+				<br/>
+				$ 200
+			</td>
+		</tr>
+		<tr>
+			<td>
+				Fax
+			</td>
+			<td>
+				Imagen
+				<br/>
+				Fax
+			</td>
+			<td>
+				Imagen
+				<br/>
+				Embajada
+			</td>
+			<td>
+				Embajada
+			</td>
+		</tr>
+		<tr>
+			<td>
+				Francia
+				<br/>
+				$ 320
+			</td>
+			<td>
+				Bandera
+				<br/>
+				Francia
+			</td>
+			<td>
+				Color segmento
+				<br/>
+				europeo dos
+				<br/>
+				Francia
+			</td>
+			<td>
+				Color segmento
+				<br/>
+				africano uno
+				<br/>
+				Marruecos
+			</td>
+			<td>
+				Bandera
+				<br/>
+				Marruecos
+			</td>
+			<td>
+				Marruecos
+				<br/>
+				$ 180
+			</td>
+		</tr>
+		<tr>
+			<td>
+				Inglaterra
+				<br/>
+				$ 350
+			</td>
+			<td>
+				Bandera
+				<br/>
+				Inglaterra
+			</td>
+			<td>
+				Color segmento
+				<br/>
+				europeo dos
+				<br/>
+				Inglaterra
+			</td>
+			<td>
+				Color segmento
+				<br/>
+				africano uno
+				<br/>
+				Libia
+			</td>
+			<td>
+				Bandera
+				<br/>
+				Libia
+			</td>
+			<td>
+				Libia
+				<br/>
+				$ 150
+			</td>
+		</tr>
+		<tr>
+			<td>
+				Méxicow
+			</td>
+			<td>
+				Color segmento
+				<br/>
+				americano uno
+				<br/>
+				Estados Unidos
+			</td>
+			<td>
+				Color segmento
+				<br/>
+				americano uno
+				<br/>
+				El Salvador
+			</td>
+			<td>
+				Imagen
+				<br/>
+				Visas
+			</td>
+			<td>
+				Color segmento
+				<br/>
+				americano uno
+				<br/>
+				Panamá
+			</td>
+			<td>
+				Imagen Línea
+				<br/>
+				Aérea americana
+			</td>
+			<td>
+				Color segmento
+				<br/>
+				americano dos
+				<br/>
+				Venezuela
+			</td>
+			<td>
+				Color segmento
+				<br/>
+				americano dos
+				<br/>
+				Brazil
+			</td>
+			<td>
+				Imagen
+				<br/>
+				E-mail
+			</td>
+			<td>
+				Color segmento
+				<br/>
+				americano dos
+				<br/>
+				Argentina
+			</td>
+			<td>
+				Deportado
+			</td>
+		</tr>
+		<tr>
+		<td>
+				Bandera
+				<br/>
+				Estados Unidos
+			</td>
+			<td>
+				Bandera
+				<br/>
+				El Salvador
+			</td>
+			<td>
+				Visas
+				<br/>
+				$ 100
+			</td>
+			<td>
+				Bandera
+				<br/>
+				Panamá
+			</td>
+			<td>
+				Línea Aérea
+				<br/>
+				Americana
+				<br/>
+				$ 200
+			</td>
+			<td>
+				Bandera
+				<br/>
+				Venezuela
+			</td>
+			<td>
+				Bandera
+				<br/>
+				Brazil
+			</td>
+			<td>
+				E-mail
+			</td>
+			<td>
+				Bandera
+				<br/>
+				Argentina
+			</td>
+		</tr>
+		<tr>
+		<td>
+				Estados
+				<br/>
+				Unidos
+				<br/>
+				$ 50
+			</td>
+			<td>
+				El Salvador
+				<br/>
+				$ 50
+			</td>
+			<td>
+				Panamá
+				<br/>
+				$ 80
+			</td>
+			<td>
+				Venezuela
+				<br/>
+				$ 120
+			</td>
+			<td>
+				Brazil
+				<br/>
+				$ 150
+			</td>
+			<td>
+				Argentina
+				<br/>
+				$ 180
+			</td>
+		</tr>
+	</tbody>
+</table>
 </body>
 </html>
